30e32947d9d4eab553f7d194db58d983014ca87f
Matt Holloway <hollowmatt@gmail.com>
2018-11-20 21:48:20 -0500

    handle volume and speed sliders

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -21,8 +21,18 @@ function updateButton() {
 	toggle.textContent = icon;
 }
 
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+	video[this.name] = this.value;
+}
+
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
