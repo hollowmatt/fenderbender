1a3e7c336a572acb7ef10d9222a42d613047f037
Matt Holloway <hollowmatt@gmail.com>
2018-11-20 21:34:20 -0500

    Update button based on vid playback

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -16,6 +16,13 @@ function togglePlay() {
 	}
 }
 
+function updateButton() {
+	const icon = this.paused ? '►' : '❚ ❚';
+	toggle.textContent = icon;
+}
+
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
