a630a85da0a4c3b323274f6103e24325a738331a
Matt Holloway <hollowmatt@gmail.com>
2018-11-23 08:57:32 -0500

    add checkSlide functionality

--- 13 - Slide in on Scroll/script.js
+++ 13 - Slide in on Scroll/script.js
@@ -18,7 +18,25 @@ function debounce(func, wait = 20, immediate = true) {
 const sliderImages = document.querySelectorAll('.slide-in');
 
 function checkSlide(e) {
-	console.count(e);
+	sliderImages.forEach(sliderImage => {
+		// scrollY tells you pixels from top
+		// innerHeight tells height of veiwport
+		// add them to get the location of bottom of vewport in ralation to page
+		// sliderImage.height gives height of image
+		const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+		//sliderImage.offsetTop tells where top of image is in relation to top of page
+		const imageBottom = sliderImage.offsetTop + sliderImage.height;
+		const isHalfShown = slideInAt > sliderImage.offsetTop;
+		const isNotScrolledPast = window.scrollY < imageBottom;
+
+		if(isHalfShown && isNotScrolledPast) {
+			sliderImage.classList.add('active');
+		} else {
+			sliderImage.classList.remove('active');
+		}
+
+	});
 }
 
+//debounce will limit the number of times the scroll event fires
 window.addEventListener('scroll', debounce(checkSlide));
